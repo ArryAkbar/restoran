87bb6dcd4599db2e3ad26259ebc16104f0685cb2
Arry Akbar <arry.ripai@rocketmail.com>
2015-11-24 23:05:57 +0700

    arry

---
+++ aboutus.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div id="wrapper">
+					
+			<div id="header">
+				<h1 id="logo">Mini Resto</h1>
+				<nav>
+					<ul id="menu">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="#">Daftar Menu</a></li>
+						<li><a href="aboutus.php">About Us</a></li>
+						<li><a href="login.php">Login</a></li>
+							
+					</ul>
+				</nav>
+				
+		</div> <!-- Div Penutup Header -->
+
+		<div id="pembatas">
+		
+		</div>
+
+	<div id="us">
+	<center><h2>Profi Mini Resto</h2></center>
+		<p>Mini Resto adalah sebuah perusahaan yang bergerak dalam bidang penyediaan makanan, berdiri sejak tanggal 20 November 2015
+		dimana pemilik perusahaan ini seorang remaja berumur 20 tahun yang bernama Arry Akbar Rifai, dia seorang anak muda yang telah 
+		berhasil membangun sebuah perusahaan dalam bidang IT. Yang kini dia mulai mencoba peruntungannya dalam bidang kuliner</p>
+	</div>
+
+		<div id ="footer">
+				<p>&copy; copyright 2015 by arryakbar.com</p>
+				<ul id="footer-menu">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#">Kontak</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
+				</ul>
+			</div> <!-- Div Penutup Footer -->
+
+		</div> <!-- Div Penutup Wrapper -->
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mini Resto</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body
+	<div id="wrapper">
+					
+			<div id="header">
+				<h1 id="logo">Mini Resto</h1>
+				<nav>
+					<ul id="menu">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="#">Daftar Menu</a></li>
+						<li><a href="aboutus.php">About Us</a></li>
+						
+							
+					</ul>
+				</nav>
+				
+		</div> <!-- Div Penutup Header -->
+
+		<div id="pembatas">
+		
+		</div>
+
+	<div id="login">
+	<h2> Silahkan masukkan username dan password untuk login..!! </h2><br>
+		
+			<p>Username : <input type="text"name="username"id="username"></p><br>
+			<p>Password : <input type="password"name="password"id="password"></p><br>
+			<input type="submit"name="submit"value="login"><br>
+
+
+
+	</div> <!-- div penutup login -->
+
+
+		<div id ="footer">
+				<p>&copy; copyright 2015 by arryakbar.com</p>
+				<ul id="footer-menu">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#">Kontak</a></li>
+					<li><a href="aboutus.php">About Us</a></li>
+				</ul>
+			</div> <!-- Div Penutup Footer -->
+
+		</div> <!-- Div Penutup Wrapper -->
+
+</body>
+</html>
